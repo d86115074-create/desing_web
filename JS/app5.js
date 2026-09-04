cfc7b2abafd41c5a7966dd44dccd496c6f5e9ac8
d86115074-create <d86115074@gmail.com>
2026-09-04 13:53:23 -0300

    67

--- JS/app5.js
+++ JS/app5.js
@@ -1,11 +1,16 @@
 function Vezes() {
 
-let vezes = prompt("Digite a quantidade de vezes")
-vezes = Number(vezes)
-let i = 1;
-while(i <= vezes){
-    alert(`contei ${i} vez`);
-    i = i + 1;
-}
-
+    let vezes = prompt("Digite a quantidade de vezes")
+    vezes = Number(vezes)
+    let i = 1;
+    while (i <= vezes) {
+        if (i == 67) {
+            alert("Sixxxxxxxxx seveeeeeeeeennnnnn Aura 9999+🗿🥶");
+        } else if (i == 69) {
+            alert("kkkkkk meia novi kkkk");
+        } else {
+            alert(`contei ${i} vez`);
+        }
+        i = i + 1;
+    }
 }
