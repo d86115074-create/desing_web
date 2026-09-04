33465247eeba3bd5490ff4a634a30695fe543b21
d86115074-create <d86115074@gmail.com>
2026-09-04 13:44:50 -0300

    while

---
+++ JS/app5.js
@@ -0,0 +1,11 @@
+function Vezes() {
+
+let vezes = prompt("Digite a quantidade de vezes")
+vezes = Number(vezes)
+let i = 1;
+while(i <= vezes){
+    alert(`contei ${i} vez`);
+    i = i + 1;
+}
+
+}
