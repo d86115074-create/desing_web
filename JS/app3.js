b9d8a187d1edd4a27c27f740fb9f704f78f90f9b
d86115074-create <d86115074@gmail.com>
2026-09-04 13:26:51 -0300

    aaaaaaaaaaaaaaa

--- JS/app3.js
+++ JS/app3.js
@@ -3,7 +3,7 @@ let num = Number(prompt("Digite um número:"));
     if (num % 2 != 0) {
         alert(`O número ${num} é ímpar`);
     }else if(num == 0){
-        alert(`O número ${num} é zero`);
+        alert(`O número ${num} é neutro`);
     }else{
         alert(`O número ${num} é par`);
     }
